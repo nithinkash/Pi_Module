529cf143cf19e2d5122fb6833f23b664dc7370d7
Nithin Kashyap M R <nithin.kashyap@bounceshare.com>
2021-07-10 20:31:03 +0530

    chnages

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from RPI import rpi_connect
+from flask_cors import CORS, cross_origin
+import threading
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/heartbeat', methods=['GET'])
@@ -11,8 +15,14 @@ def heart_beat():
 
 @app.route('/gpio_on', methods=['GET'])
 def turn_on_led():
-    # rpi_connect
-    return
+    thread = threading.Thread(target=pi_thread)
+    thread.start()
+    return "OK"
+
+
+def pi_thread():
+    rpi_connect.on_notifier()
+
 
 if __name__ == '__main__':
     app.run()
